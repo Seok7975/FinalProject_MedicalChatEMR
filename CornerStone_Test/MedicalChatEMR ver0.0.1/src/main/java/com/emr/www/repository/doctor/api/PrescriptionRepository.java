5ff64a6af0a2a60fe30d930ce762071f78f48661
junhyeoklim <gur710@gmail.com>
2024-08-19 21:11:29 +0900

    의사 페이지 연동중

---
+++ CornerStone_Test/MedicalChatEMR ver0.0.1/src/main/java/com/emr/www/repository/doctor/api/PrescriptionRepository.java
@@ -0,0 +1,10 @@
+package com.emr.www.repository.doctor.api;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emr.www.entity.doctor.PrescriptionEntity;
+
+@Repository
+public interface PrescriptionRepository extends JpaRepository<PrescriptionEntity, Integer> {
+//dao 역할
+}
